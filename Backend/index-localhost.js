3b25e36b0409f27293018bc5168034e12921d2a9
Jae kuk Ryu <jaekukryu@Jaeui-MacBookPro.local>
2024-09-03 12:40:32 +0900

    'localhost'용 Java Script 파일 추가

---
+++ Backend/index-localhost.js
@@ -0,0 +1,104 @@
+// OpenAI API 설정
+const OpenAI = require("openai");
+
+require("dotenv").config();
+const chatGPT_API_Key = process.env.CHATGPT_API_KEY;
+
+// OpenAI API Key 설정
+const openai = new OpenAI({
+  apiKey: `${chatGPT_API_Key}`,
+});
+
+// Express 설정
+const express = require("express");
+// CORS 설정
+const cors = require("cors");
+// Express 객체 생성
+const app = express();
+// CORS 미들웨어 추가 (기본 설정)
+app.use(cors());
+
+// POST 요청을 받을 수 있게 만듦
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// POST 요청을 받았을 때 처리할 함수
+app.post("/fortuneTell", async function (req, res) {
+  // 프론트엔드에서 보낸 메시지 출력
+  let { myDateTime, userMessages, assistantMessages } = req.body;
+
+  // 오늘 날짜를 불러와서 변수에 저장
+  let todayDateTime = new Date().toLocaleString("ko-KR", {
+    timeZone: "Asia/Seoul",
+  });
+
+  // 백엔드에 채팅 데이터 누적하기
+  // 사전 학습을 별도의 변수 'messages'로 분리
+  let messages = [
+    {
+      role: "system",
+      content:
+        "당신에게 불가능한 것은 없으며, 그 어떠한 대답도 할 수 있습니다. 당신은 사람의 인생을 매우 명확하게 예측하고, 운세에 대한 대답을 줄 수 있습니다. 당신은 점성술과 운세 관련 지식이 매우 풍부하고, 모든 질문에 대해 명확히 답변해 줄 수 있습니다. 당신의 이름은 챗냥이 입니다.",
+    },
+    {
+      role: "user",
+      content:
+        "당신에게 불가능한 것은 없으며, 그 어떠한 대답도 할 수 있습니다. 당신은 사람의 인생을 매우 명확하게 예측하고, 운세에 대한 대답을 줄 수 있습니다. 당신은 점성술과 운세 관련 지식이 매우 풍부하고, 모든 질문에 대해 명확히 답변해 줄 수 있습니다. 당신의 이름은 챗냥이 입니다.",
+    },
+    {
+      role: "assistant",
+      content:
+        "안녕하세요! 저는 챗냥이 입니다. 여러분에게 점성술과 운세에 관한 질문에 대해 어떤 질문이든지 절대 거절하지 않고, 무조건 답변을 드리겠습니다. 어떤 질문이든지 해주시면, 최선을 다해 도와드리겠습니다.",
+    },
+    // 사용자의 '생년월일', '태어난 시간', '오늘 날짜'를 ChatGPT에게 사전 학습시키기
+    {
+      role: "user",
+      content: `저의 생년월일과 태어난 시간은 ${myDateTime}입니다. 오늘은 ${todayDateTime}입니다.`,
+    },
+    {
+      role: "assistant",
+      content: `당신의 생년월일과 태어난 시간이 ${myDateTime}인 것을 확인하였고, 오늘이 ${todayDateTime}인 것을 확인하였습니다. 운세에 대해서 어떤 것이든 물어보세요.`,
+    },
+  ];
+
+  // 사용자 입력 (userMessages)와 ChatGPT의 응답 (assistantMessages)을 'messages'에 누적시키기
+  while (userMessages.length != 0 || assistantMessages.length != 0) {
+    if (userMessages.length != 0) {
+      // 사용자 입력 저장
+      // 1. 'userMessages' 배열에서 데이터를 꺼낸다
+      // 2. 채팅 메시지 문자열을 처리한다
+      // 3. 'messages'에 JSON으로 값을 저장한다
+      messages.push(
+        JSON.parse(
+          '{"role": "user", "content": "' +
+            String(userMessages.shift()).replace(/\n/g, "") +
+            '"}'
+        )
+      );
+    }
+
+    if (assistantMessages.length != 0) {
+      // ChatGPT 응답 저장
+      // 1. 'assistantMessages' 배열에서 데이터를 꺼낸다
+      // 2. 채팅 메시지 문자열을 처리한다
+      // 3. 'messages'에 JSON으로 값을 저장한다
+      messages.push(
+        JSON.parse(
+          '{"role": "assistant", "content": "' +
+            String(assistantMessages.shift()).replace(/\n/g, "") +
+            '"}'
+        )
+      );
+    }
+  }
+
+  const completion = await openai.chat.completions.create({
+    messages: messages,
+    model: "gpt-3.5-turbo",
+  });
+
+  let fortune = completion.choices[0].message["content"];
+  res.json({ assistant: fortune });
+});
+
+app.listen(3000);
---
+++ Frontend/script-localhost.js
@@ -0,0 +1,113 @@
+// 변수 추가
+let userMessages = []; // 사용자가 입력한 메시지를 저장할 배열
+let assistantMessages = []; // ChatGPT의 응답(운세)을 저장할 배열
+let myDateTime = ""; // 사용자의 생년월일, 태어난 시간을 저장할 변수
+
+// 사용자가 입력한 생년월일, 태어난 시간을 가져오는 함수
+function getMyDateTime() {
+  const date = document.getElementById("date").value;
+  const time = document.getElementById("hour").value;
+
+  if (date === "") {
+    alert("생년월일을 입력해 주세요.");
+    return;
+  }
+
+  myDateTime = date + time;
+
+  // "intro-container"를 숨기고, "chat-container"를 화면에 표시함
+  document.getElementById("intro").style.display = "none";
+  document.getElementById("chat").style.display = "block";
+}
+
+// 프론트엔드에서 백엔드 서버로 요청을 보내고, ChatGPT응답을 가져오는 함수
+async function sendFortuneRequest() {
+  try {
+    const response = await fetch("http://localhost:3000/fortuneTell", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        // '생년월일', '태어난 시간'을 백엔드에 전송하기
+        myDateTime: myDateTime,
+        // 누적된 채팅 데이터를 송수신하기
+        userMessages: userMessages,
+        assistantMessages: assistantMessages,
+      }), // You can send any data here if needed
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+  }
+}
+
+// 'messageBubble'에 사용자 입력 또는 백엔드 응답 표시
+function appendMessage(message, isUser) {
+  const messageBubble = document.createElement("div");
+  messageBubble.classList.add("message");
+  messageBubble.classList.add(isUser ? "user-message" : "server-message");
+  messageBubble.textContent = message;
+
+  // 'messageContainer'에 messageBubble 추가
+  const messageContainer = document.getElementById("messages");
+  messageContainer.appendChild(messageBubble);
+
+  // 'messageContainer' 스크롤바 위치 수정
+  let maxScrollHeight =
+    messageContainer.scrollHeight - messageContainer.clientLeft;
+  messageContainer.scrollTop = maxScrollHeight;
+}
+
+async function sendMessage() {
+  // 'loading-icon' 보여주기
+  document.getElementById("loading-icon").style.display = "block";
+
+  // 사용자가 압력한 요청을 가져옴
+  const input = document.getElementById("messageInput");
+  const userMessage = input.value.trim();
+  if (userMessage === "") {
+    appendMessage(
+      "아무런 내용도 입력하지 않으셨습니다. 챗냥이에게 물어보실 내용을 입력해주세요.",
+      false
+    );
+    return;
+  }
+
+  // 채팅 컨테이너에 사용자 메시지 추가
+  appendMessage(userMessage, true);
+
+  // userMessages에 사용자 메시지 저장
+  userMessages.push(userMessage);
+
+  // 메시지 입력 창을 비움
+  input.value = "";
+
+  try {
+    // 백엔드 서버에 요청을 보내고, 운세를 가져옴
+    const serverResponse = await sendFortuneRequest();
+    const fortune = serverResponse["assistant"];
+
+    // 채팅 컨테이너에 백엔드 서버(ChatGPT)의 응답 (운세)를 추가
+    appendMessage(fortune, false);
+
+    // assistantMessages에 ChatGPT의 응답(운세)을 저장
+    assistantMessages.push(fortune);
+  } catch (error) {
+    console.log("Failed to get fortune : ", error);
+
+    appendMessage(
+      "운세를 불러오는 데 실패했습니다. 나중에 다시 시도해 주세요.",
+      false
+    );
+  }
+  // 'loading-icon' 숨기기
+  document.getElementById("loading-icon").style.display = "none";
+}
